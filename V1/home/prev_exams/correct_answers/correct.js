5d5527d7c686b8245e8327906bb5b851f9bc3aac
mohamedhw <mohamedhw2000@gmail.com>
2025-01-28 19:19:32 +0200

    v4

--- V1/home/prev_exams/correct_answers/correct.js
+++ V1/home/prev_exams/correct_answers/correct.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const user = JSON.parse(localStorage.getItem("loggedInUser"));
+  const user = JSON.parse(localStorage.getItem("logedin"));
   if (user) {
     document.getElementById("user-name-text").innerText =
       user.firstName + " " + user.lastName;
-    loadCorrectAnswers(user.email);
+    loadCorrectAnswers(user);
   } else {
     window.location.href = "../../../users/login/login.html";
   }
 });
 
-function loadCorrectAnswers(userEmail) {
-  const users = JSON.parse(localStorage.getItem("users")) || [];
-  const user = users.find((u) => u.email === userEmail);
+function loadCorrectAnswers(user) {
   const examIndex = localStorage.getItem("examIndex");
   if (user && user.exams && user.exams[examIndex]) {
     const exam = user.exams[examIndex];
     const answersTableBody = document.getElementById("answers-table-body");
+    console.log(exam)
     exam.answers.forEach((answer, index) => {
       const question = exam.questions[index];
       const row = document.createElement("tr");
